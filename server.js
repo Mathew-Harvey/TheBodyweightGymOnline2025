6428b8c9e51e9c579ecd415a59d23eca9effab5e
Mathew Harvey <mathewharvey@gmai.com>
2025-08-09 18:25:37 +0800

    changed server port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3004;
 
 // Security middleware
 app.use(helmet({
